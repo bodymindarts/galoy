a5c5cea7478fb897462354d5baf913d105f8a375
Kartik Shah <krtk.6160@gmail.com>
2021-06-10 13:15:36 +0530

    disable sentinel

--- src/redis.ts
+++ src/redis.ts
@@ -12,14 +12,19 @@ if(process.env.LOCAL === 'true') {
 
 }
 
+// connectionObj = {
+//   sentinels: [
+//     { host: `${process.env.REDIS_0_DNS}`, port: process.env.REDIS_0_SENTINEL_PORT || 26379 },
+//     { host: `${process.env.REDIS_1_DNS}`, port: process.env.REDIS_1_SENTINEL_PORT || 26379 },
+//     { host: `${process.env.REDIS_2_DNS}`, port: process.env.REDIS_2_SENTINEL_PORT || 26379 },
+//   ],
+//   name: process.env.REDIS_MASTER_NAME ?? "mymaster",
+//   natMap
+// }
+
 connectionObj = {
-  sentinels: [
-    { host: `${process.env.REDIS_0_DNS}`, port: process.env.REDIS_0_SENTINEL_PORT || 26379 },
-    { host: `${process.env.REDIS_1_DNS}`, port: process.env.REDIS_1_SENTINEL_PORT || 26379 },
-    { host: `${process.env.REDIS_2_DNS}`, port: process.env.REDIS_2_SENTINEL_PORT || 26379 },
-  ],
-  name: process.env.REDIS_MASTER_NAME ?? "mymaster",
-  natMap
+  port: 6379,
+  host: process.env.REDIS_0_DNS
 }
 
 export const redis = new Redis(connectionObj);
